6696067f758718dce8bb12f68a805b4eab4428ff
jabrille+1-at-755450188907 <Jaime.Abrille@nttdatafed.com>
2024-04-23 02:43:46 -0400

    finished day 17 - quiz game

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 
-for question in question_data:
-    question_bank.append(question)
-    print(question)
+for dict in question_data:
+    text = dict["question"]
+    answer = dict["correct_answer"]
+    question_bank.append(Question(text, answer))
 
-print(question_bank)
+    q_brain = QuizBrain(question_bank)
+
+while q_brain.still_has_questions():
+    q_brain.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {q_brain.score}/{len(q_brain.questions_list)}")
